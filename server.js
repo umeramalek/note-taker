f400ce5883b1ee97fb10c397af685046a0d58b43
Umera <umeramalek796@gmail.com>
2021-10-20 18:04:31 -0700

    added app.post method

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3001;
 const app = express();
 const path = require('path');
 const util = require('util');
+const uuid = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,13 +18,6 @@ app.get("/notes", (req,res)=>{
     res.sendFile(htmlFile)
 })
 
-app.get("*", (req,res)=>{
-    let htmlFile = path.join(__dirname, '/public/index.html')
-    console.log(htmlFile)
-    res.sendFile(htmlFile)
-})
-
-
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
@@ -32,6 +26,41 @@ app.get("/api/notes", (req,res)=>{
     res.send(jsonData)
     readFromFile(jsonData).then((data) => res.json(JSON.parse(data)));
 })
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+
+app.post("/api/notes", (req,res)=>{
+    const { title,text } = req.body;
+
+
+    readAndAppend(newTip, './db/tips.json');
+})
+
+
+
+app.get("*", (req,res)=>{
+    let htmlFile = path.join(__dirname, '/public/index.html')
+    console.log(htmlFile)
+    res.sendFile(htmlFile)
+})
+
+
 
 
 
